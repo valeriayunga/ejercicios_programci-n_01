bdd7e9b9eb75eed8edf73622a50481e1aae796ce
Valeria Yunga <vayunga@utpl.edu.ec>
2021-10-22 11:31:47 -0500

    primer commit

---
+++ proyecto_01/src/proyecto_01/Proyecto_01.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_01;
+
+/**
+ *
+ * @author utpl
+ */
+public class Proyecto_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Ejemplo básico de Java  ");
+    System.out.print("Versionado a través de Git ");
+    System.out.print("Manejado desde GitHub ");
+    }
+    
+}
